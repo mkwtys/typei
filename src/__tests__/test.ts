463099ba46c55a47272072450b2a9354d8c5e505
mkwtys <mkwtys@gmail.com>
2019-10-11 13:01:17 +0900

    implements detect package manager

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -5,39 +5,42 @@ import path from 'path'
 import rimraf from 'rimraf'
 import { interactiveUpdate } from '../'
 
-describe('actions', () => {
-  const cwd = path.join(__dirname, 'testProject')
-  const actualPackagePath = path.join(cwd, 'package.json')
+const cwd = path.join(__dirname, 'testProject')
+const actualPackagePath = path.join(cwd, 'package.json')
+const packageManagers = ['npm', 'yarn']
+const fixtures = [
+  'has-index-types',
+  'has-types',
+  'has-typings',
+  'installed-types',
+  'no-deps',
+  'not-registered-in-types-registry',
+  'scoped-package'
+]
 
-  function test(fixtureName) {
-    it(`${fixtureName}`, async () => {
-      jest.setTimeout(60000)
-      rimraf.sync(cwd)
-      mkdirp.sync(cwd)
-      const expectedPackage = require(`./fixtures/${fixtureName}/expected.json`)
-      const fixturePackage = require(`./fixtures/${fixtureName}/fixture.json`)
+function test(packageManager, fixtureName) {
+  it(`${packageManager}: ${fixtureName}`, async () => {
+    jest.setTimeout(60000)
+    rimraf.sync(cwd)
+    mkdirp.sync(cwd)
+    const expectedPackage = require(`./fixtures/${fixtureName}/expected.json`)
+    const fixturePackage = require(`./fixtures/${fixtureName}/fixture.json`)
+    const dir = cwd ? `cd ${cwd} &&` : ''
+    const command = `${dir} ${packageManager} install`
 
-      fs.writeFileSync(actualPackagePath, JSON.stringify(fixturePackage))
+    fs.writeFileSync(actualPackagePath, JSON.stringify(fixturePackage))
+    await execa.command(command, { env: { ...process.env }, shell: true })
+    await interactiveUpdate({ cwd, update: true })
 
-      const dir = cwd ? `cd ${cwd} &&` : ''
+    const actualPackage = JSON.parse(fs.readFileSync(actualPackagePath, { encoding: 'utf8' }))
+    expect(Object.keys({ ...actualPackage.dependencies, ...actualPackage.devDependencies }).sort()).toEqual(
+      Object.keys({ ...expectedPackage.dependencies, ...expectedPackage.devDependencies }).sort()
+    )
+  })
+}
 
-      // run `npm install` to get dependencies information from node_modules
-      await execa.command(`${dir} npm install`, { env: { ...process.env }, shell: true })
-
-      await interactiveUpdate({ cwd, update: true })
-      const actualPackage = JSON.parse(fs.readFileSync(actualPackagePath, { encoding: 'utf8' }))
-
-      expect(Object.keys({ ...actualPackage.dependencies, ...actualPackage.devDependencies }).sort()).toEqual(
-        Object.keys({ ...expectedPackage.dependencies, ...expectedPackage.devDependencies }).sort()
-      )
-    })
-  }
-
-  test('has-index-types')
-  test('has-types')
-  test('has-typings')
-  test('installed-types')
-  test('no-deps')
-  test('not-registered-in-types-registry')
-  test('scoped-package')
+packageManagers.forEach(packageManager => {
+  fixtures.forEach(fixture => {
+    test(packageManager, fixture)
+  })
 })
--- src/installPackages.ts
+++ src/installPackages.ts
@@ -1,14 +1,23 @@
 import execa from 'execa'
 import ora from 'ora'
+import preferredPM from 'preferred-pm'
 import { Summary } from './types'
+
+async function getPreferredPackageManager({ cwd }: { cwd: string }) {
+  const supported = ['npm', 'yarn']
+  const pm = await preferredPM(cwd)
+  return pm && supported.indexOf(pm.name) !== -1 ? pm.name : 'npm'
+}
 
 export async function installPackages({ cwd, packageSummary }: { cwd?: string; packageSummary: Summary[] }) {
   const dir = cwd ? `cd ${cwd} &&` : ''
+  const packageManager = await getPreferredPackageManager({ cwd: process.cwd() })
   const packages = packageSummary.map(summary => `${summary.typesName}@${summary.latest}`).join(' ')
+  const command = `${dir} ${packageManager} ${packageManager === 'yarn' ? 'add' : 'install'} --save-dev ${packages}`
   const spinner = ora(`install types...`)
 
   spinner.start()
-  const output = await execa.command(`${dir} npm install --save-dev ${packages}`, {
+  const output = await execa.command(command, {
     env: { ...process.env },
     shell: true
   })
