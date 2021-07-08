34cc06e27b447fefccb8d22c7cf9454afcef2dba
mkwtys <mkwtys@gmail.com>
2021-07-08 11:44:26 +0900

    Update test.ts

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -18,7 +18,7 @@ const fixtures = [
   'scoped-package'
 ]
 
-jest.setTimeout(20000)
+jest.setTimeout(30000)
 
 function test(packageManager, fixtureName) {
   it(`${packageManager}: ${fixtureName}`, async () => {
